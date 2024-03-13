4ca65eacc097dd59dce72a1157a7f4ab7f990272
nenowo <38612806+nenowoDev@users.noreply.github.com>
2024-03-13 17:52:14 +0800

    improve link searching

--- content.js
+++ content.js
@@ -3,32 +3,43 @@ function processLinks() {
     const currentUrl = window.location.href;  
     const links = Array.from(document.querySelectorAll('a'));  
     const filteredLinks = links.filter(link => link.href !== currentUrl);
-    const link1080p = filteredLinks.find(link => link.href.includes('pahe.win') && link.innerText.includes('1080p'));
-    const linkkwik = filteredLinks.find(link => link.href.includes('kwik.si/f'));
+    const foundlink1080p = filteredLinks.find(link => link.href.includes('pahe.win') && link.innerText.includes('1080p'));
+    const foundlinkkwik = filteredLinks.find(link => link.href.includes('kwik.si/f'));
 
-    const timeoutspann=2000;
+    const timeoutspann=1000;
 
-    if (link1080p) {
-        console.log(`Found link with 1080p: ${link1080p.href}`);
-        window.open(link1080p.href, '_blank').focus;
+    if (foundlink1080p) {
+        console.log(`Found link with 1080p: ${foundlink1080p.href}`);
+        window.open(foundlink1080p.href, '_blank').focus;
         setTimeout(() => {
             window.close();
         }, timeoutspann);
     }
-  
+     
     
-    
-    
-    if (linkkwik) {
-        console.log(`Found link with kwikcx: ${linkkwik.href}`);
-        window.open(linkkwik.href, '_blank').focus;
+    if (foundlinkkwik) {
+        console.log(`Found link with kwikcx: ${foundlinkkwik.href}`);
+        window.open(foundlinkkwik.href, '_blank').focus;
         setTimeout(() => {
             window.close();
-        }, timeoutspann);
+        }, timeoutspann); 
         
     }
 
-    
+
+    if(currentUrl.href.includes('pahe.win')){
+        setInterval(() => {
+            foundlinkkwik = filteredLinks.find(link => link.href.includes('kwik.si/f'));
+            if (foundlinkkwik) {
+                clearInterval(intervalId);
+                console.log('Link found:', linkkwik.href);
+            } else {
+                console.log('Link not found. Retrying in 5 seconds...');
+            }
+        }, 5000); 
+    }
+
+    //download
     if (currentUrl.includes('kwik.si')) {
         const button = document.querySelector('button');
 
