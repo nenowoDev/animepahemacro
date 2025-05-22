0a878a1ffe19783c01f1373ad1e797b9ac673ab0
nenowo <38612806+nenowoDev@users.noreply.github.com>
2025-05-22 22:12:29 +0800

    better selection

--- content.js
+++ content.js
@@ -1,33 +1,52 @@
 function processLinks() {
-    
-    const currentUrl = window.location.href;  
-    const links = Array.from(document.querySelectorAll('a'));  
+
+    const currentUrl = window.location.href;
+    const links = Array.from(document.querySelectorAll('a'));
     const filteredLinks = links.filter(link => link.href !== currentUrl);
-    const foundlink1080p = filteredLinks.find(link => link.href.includes('pahe.win') && link.innerText.includes('1080p'));
+    let selectedLink = null;
+
+    // Try to find a 1080p link first
+    selectedLink = filteredLinks.find(link =>
+        link.href.includes('pahe.win') && link.innerText.includes('1080p')
+    );
+
+    // If 1080p not found, try to find a 720p link
+    if (!selectedLink) {
+        selectedLink = filteredLinks.find(link =>
+            link.href.includes('pahe.win') && link.innerText.includes('720p')
+        );
+    }
+
+    // If 720p not found, try to find a 480p link
+    if (!selectedLink) {
+        selectedLink = filteredLinks.find(link =>
+            link.href.includes('pahe.win') && link.innerText.includes('480p')
+        );
+    }
     var foundlinkkwik = filteredLinks.find(link => link.href.includes('kwik.si/f'));
 
-    const timeoutspann=1000;
+    const timeoutspann = 1000;
 
-    if (foundlink1080p) {
-        console.log(`Found link with 1080p: ${foundlink1080p.href}`);
-        window.open(foundlink1080p.href, '_blank').focus;
+    if (selectedLink) {
+        console.log(`Found link with 1080p: ${selectedLink.href}`);
+        window.open(selectedLink.href, '_blank').focus;
         setTimeout(() => {
             window.close();
         }, timeoutspann);
     }
-     
-    
+
+
     if (foundlinkkwik) {
         console.log(`Found link with kwikcx: ${foundlinkkwik.href}`);
         window.open(foundlinkkwik.href, '_blank').focus;
         setTimeout(() => {
             window.close();
-        }, timeoutspann); 
-        
-    }
-    
+        }, timeoutspann);
 
-    if(currentUrl.includes('pahe.win')){
+    }
+
+
+    if (currentUrl.includes('pahe.win')) {
         setInterval(() => {
             foundlinkkwik = filteredLinks.find(link => link.href.includes('kwik.si/f'));
             if (foundlinkkwik) {
@@ -35,12 +54,12 @@ function processLinks() {
                 window.open(foundlinkkwik.href, '_blank').focus;
                 setTimeout(() => {
                     window.close();
-                }, timeoutspann); 
+                }, timeoutspann);
             }
             else {
                 console.log('Link not found. Retrying in 5 seconds...');
             }
-        }, 5000); 
+        }, 5000);
     }
 
     //download
@@ -55,7 +74,7 @@ function processLinks() {
         }
     }
 
-   
+
 }
 
 
